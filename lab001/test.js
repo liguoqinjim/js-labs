f194e7973b9c85124e3605e0d329dbedbf5ede35
liguoqinjim <liguoqinjim23@gmail.com>
2017-01-23 14:49:36 +0800

    lab001

---
+++ lab001/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by admin on 2017/1/23.
+ */
+
+var a = "0102030405060708";
+for (var c = a.length, e = [], j = 0; j < c; j++) {
+    e[j >>> 2] |= (a.charCodeAt(j) & 255) << 24 - 8 * (j % 4);
+    // console.log((a.charCodeAt(j) & 255) << 24 - 8 * (j % 4));
+}
+console.log(e);
