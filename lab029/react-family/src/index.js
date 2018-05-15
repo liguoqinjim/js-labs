8ef44fdf2ed9cdd8c78aed11126333d0233d0224
liguoqinjim <liguoqinjim23@gmail.com>
2018-05-15 19:27:01 +0800

    react-router-dom

--- lab029/react-family/src/index.js
+++ lab029/react-family/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Hello from './compoent/Hello/Hello'
+
+import getRouter from './router/router'
 
 ReactDom.render(
-    <Hello/>, document.getElementById('app'));
+    getRouter(), document.getElementById("app")
+)
