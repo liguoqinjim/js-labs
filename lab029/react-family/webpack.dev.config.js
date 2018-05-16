e1b15ecac0cae961620b44c018b126d6be18a77d
liguoqinjim <liguoqinjim23@gmail.com>
2018-05-16 16:41:40 +0800

    缓存

--- lab029/react-family/webpack.dev.config.js
+++ lab029/react-family/webpack.dev.config.js
@@ -13,8 +13,8 @@ module.exports = {
     /*输出到dist文件夹，输出文件名字为bundle.js*/
     output: {
         path: path.join(__dirname, './dist'),
-        filename: 'bundle.js',
-        chunkFilename: '[name].js'
+        filename: '[name].[hash].js',
+        chunkFilename: '[name].[chunkhash].js'
     },
 
     module: {
