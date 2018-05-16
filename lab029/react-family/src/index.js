3048d2613027e47a55479c8e2406f898a408c587
liguoqinjim <liguoqinjim23@gmail.com>
2018-05-16 11:57:25 +0800

    模块热替换

--- lab029/react-family/src/index.js
+++ lab029/react-family/src/index.js
@@ -3,6 +3,10 @@ import ReactDom from 'react-dom';
 
 import getRouter from './router/router'
 
+if (module.hot) {
+    module.hot.accept();
+}
+
 ReactDom.render(
     getRouter(), document.getElementById("app")
 )
