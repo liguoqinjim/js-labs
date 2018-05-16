c7df2bf25bd8601649422a0af53872ccf7dee720
liguoqinjim <liguoqinjim23@gmail.com>
2018-05-16 12:10:15 +0800

    react-hot-loader

--- lab029/react-family/webpack.dev.config.js
+++ lab029/react-family/webpack.dev.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 module.exports = {
 
     /*入口*/
-    entry: path.join(__dirname, 'src/index.js'),
+    entry: [
+        'react-hot-loader/patch',
+        path.join(__dirname, 'src/index.js')
+    ],
 
     /*输出到dist文件夹，输出文件名字为bundle.js*/
     output: {
