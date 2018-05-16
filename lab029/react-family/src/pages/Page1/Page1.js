3ccc9560c0168a56a042dffc792f846df713f32b
liguoqinjim <liguoqinjim23@gmail.com>
2018-05-16 23:44:54 +0800

    使用 CSS Modules

--- lab029/react-family/src/pages/Page1/Page1.js
+++ lab029/react-family/src/pages/Page1/Page1.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
 
-import './Page1.css'
+import style from './Page1.css';
 import image from './images/user00.jpg'
 
 export default class Page1 extends Component {
     render() {
         return (
-            <div className="page-box">
+            <div className={style.box}>
                 this is Page1~~
                 <img src={image}/>
             </div>
