67703d06d6acf01ce47b9dc9ac242bcd9e8edf02
liguoqinjim <liguoqinjim23@gmail.com>
2018-05-16 19:21:32 +0800

    使用axios和middleware优化请求

--- lab029/react-family/src/redux/actions/userInfo.js
+++ lab029/react-family/src/redux/actions/userInfo.js
@@ -22,20 +22,8 @@ function getUserInfoFail() {
 }
 
 export function getUserInfo() {
-    return function (dispatch) {
-        dispatch(getUserInfoRequest());
-
-        return fetch('http://localhost:8080/api/user.json')
-            .then((response => {
-                return response.json()
-            }))
-            .then((json) => {
-                    dispatch(getUserInfoSuccess(json))
-                }
-            ).catch(
-                () => {
-                    dispatch(getUserInfoFail());
-                }
-            )
+    return {
+        types: [GET_USER_INFO_REQUEST, GET_USER_INFO_SUCCESS, GET_USER_INFO_FAIL],
+        promise: client => client.get("http://localhost:8080/api/user.json")
     }
 }
--- lab029/react-family/src/redux/store.js
+++ lab029/react-family/src/redux/store.js
@@ -1,7 +1,8 @@
 import {createStore, applyMiddleware} from 'redux';
-import thunkMiddleware from 'redux-thunk';
 import combineReducers from './reducers.js';
 
-let store = createStore(combineReducers, applyMiddleware(thunkMiddleware));
+import promissMiddleware from './middleware/promiseMiddleware'
+
+let store = createStore(combineReducers, applyMiddleware(promissMiddleware));
 
 export default store;
