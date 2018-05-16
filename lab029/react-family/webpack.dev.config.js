9de6adc74f3a80f90014a5cf441df7a7e063e7b2
liguoqinjim <liguoqinjim23@gmail.com>
2018-05-16 19:35:57 +0800

    合并提取webpack公共配置

--- lab029/react-family/webpack.dev.config.js
+++ lab029/react-family/webpack.dev.config.js
@@ -1,73 +1,39 @@
+const merge = require('webpack-merge');
 const path = require('path');
 
-var HTMLWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
+const commonConfig = require('./webpack.common.config');
 
-module.exports = {
-    //调试工具优化
-    devtool: 'inline-source-map',
-
-    /*入口*/
+const devConfig = {
+    devtool: "inline-source-map",
     entry: {
-        app: ['react-hot-loader/patch',
-            path.join(__dirname, 'src/index.js')],
-        vendor: ['react', 'react-router-dom', 'redux', 'react-dom', 'react-redux']
+        app: [
+            'react-hot-loader/patch',
+            path.join(__dirname, "src/index.js")
+        ]
     },
-
-    /*输出到dist文件夹，输出文件名字为bundle.js*/
     output: {
-        path: path.join(__dirname, './dist'),
-        filename: '[name].[hash].js',
-        chunkFilename: '[name].[chunkhash].js'
+        /*这里本来应该是[chunkhash]的，但是由于[chunkhash]和react-hot-loader不兼容。只能妥协*/
+        filename: '[name].[hash].js'
     },
-
     module: {
         rules: [{
-            test: /\.js$/,
-            use: ['babel-loader?cacheDirectory=true'],
-            include: path.join(__dirname, 'src')
-        }, {
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }, {
-            test: /\.(png|jpg|gif)$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 8192
-                }
-            }]
+            use: ["style-loader", "css-loader"]
         }]
     },
-
-    //webpack-dev-server
     devServer: {
-        port: 8080,
         contentBase: path.join(__dirname, './dist'),
         historyApiFallback: true,
-        host: '0.0.0.0'
-    },
-
-    //别名
-    resolve: {
-        alias: {
-            pages: path.join(__dirname, "src/pages"),
-            component: path.join(__dirname, "src/component"),
-            router: path.join(__dirname, "src/router"),
-            actions: path.join(__dirname, "src/redux/actions"),
-            reducers: path.join(__dirname, "src/redux/reducers"),
-        }
-    },
-
-    //plugin
-    plugins: [
-        new HTMLWebpackPlugin({
-            filename: 'index.html',
-            template: path.join(__dirname, "src/index.html")
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor'
-        })
-    ]
+        host: '0.0.0.0',
+    }
 };
 
+//合并config
+module.exports = merge({
+    customizeArray(a, b, key) {
+        if (key === 'entry.app') {
+            return b;
+        }
+        return undefined;
+    }
+})(commonConfig, devConfig);
