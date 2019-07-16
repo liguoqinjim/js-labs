9d70ac63af6bf4b7ba673188b263a1d285ef29ff
liguoqinjim <liguoqinjim23@gmail.com>
2019-07-17 00:23:05 +0800

    update

---
+++ lab048/lab001.js
@@ -0,0 +1,6 @@
+url = "https://buy.damai.cn/orderConfirm?exParams={\"damai\":\"1\",\"channel\":\"damai_app\",\"umpChannel\":\"10002\",\"atomSplit\":\"1\",\"seatInfo\":\"4158989944399_2449208_334248264,4158989944399_2449208_334248265\",\"serviceVersion\":\"1.8.5\"}&buyParam=597632025030_2_4158989944399&buyNow=true&projectId=200792012&performId=210067122&spm=a2oeg.selectseat.bottom.dbuy";
+
+console.log("url=" + url);
+
+url = encodeURIComponent(url);
+console.log("url=" + url);
